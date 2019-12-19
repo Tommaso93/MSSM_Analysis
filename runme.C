411429365823766b0765595b4b7eb8f05e32bcea
Tommaso Diotalevi <tommaso.diotalevi@gmail.com>
2019-12-19 16:04:45 +0100

    Corrected macro for automatic execution

--- runme.C
+++ runme.C
@@ -1,9 +1,12 @@
 #include "TString.h"
 #include "TROOT.h"
+#include <string>
 
-void runme(char const *arg){
+void runme(std::string arg, float mA){
+std::string strng = "c = NewAnalyzer(\"" + arg + "\"," + std::to_string(mA) + "," + std::to_string(mA-100.) + "," + std::to_string(mA+100.) + ", 0, 0)";
 gROOT->ProcessLine(".L NewAnalyzer.C++");
-gROOT->ProcessLine(TString::Format("c = NewAnalyzer(\"%s\", 300., 200., 400., 0, 0)",arg));
+gROOT->ProcessLine(strng.c_str());
+//gROOT->ProcessLine(TString::Format("c = NewAnalyzer(\"%s\", 300., 200., 400., 0, 0)",arg));
 gROOT->ProcessLine("c.Loop()");
 gROOT->ProcessLine("c.Draw2()");
 }
